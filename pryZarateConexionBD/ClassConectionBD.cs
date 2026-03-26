a200756919b9f1f93d54e6d3b3434600e2418c39
Alumno <Alumno@PC117>
2026-03-26 09:00:08 -0300

    Agregar archivos de proyecto.

---
+++ pryZarateConexionBD/ClassConectionBD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+
+namespace pryZarateConexionBD
+{
+    internal class ClassConectionBD
+    {
+        OleDbConnection conn;
+        public void ConectarBD()
+        {
+            conn = new OleDbConnection();
+            conn.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0; Data Source=" Application.StartupPath + "\\BaseDatos\\baseJuegoRPG.accdb"; 
+            conn.Open();
+    }
+    }
+}
